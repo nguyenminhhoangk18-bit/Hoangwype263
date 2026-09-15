5fcd91eb607174e516dfd0f3e0cfab6117aec6f9
nguyenminhhoangk18-bit <nguyenminhhoangk18@siu.edu.vn>
2026-09-15 16:32:38 +0700

    Implement dynamic array input and output
    Added dynamic array allocation and user input for elements.

--- Hoang.cpp
+++ Hoang.cpp
@@ -1,1 +1,20 @@
-cout <<"Hello";
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Hay nhap so luong phan tu :";
+    cin >> n;
+    int* a = new int [n];
+    for (int i=0;i<n;i++){
+        cout << "a["<<i<<"]=";
+        cin >> a[i];
+        cout << "Hello";
+    }
+    cout <<"Mang cap phat dong vua nhap :";
+    for (int i=0;i<n;i++){
+        cout<< a[i] <<" ";
+    }
+    delete[] a;
+    return 0;
+}
